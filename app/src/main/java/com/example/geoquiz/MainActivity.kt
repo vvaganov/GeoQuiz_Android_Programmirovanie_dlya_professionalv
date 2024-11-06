c9d2d85e18998d917e7bc8fa4c1d27e8fc2098fa
Valery <i@vvaganov.ru>
2024-11-06 20:06:15 +0500

    PFP 02-str88

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var questionText: TextView
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -32,34 +34,52 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         questionText = findViewById(R.id.question_text_view)
+        prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
-
-
-        trueButton.setOnClickListener {
-            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP or Gravity.CENTER, 0, 0)
-            toast.show()
-        }
-
-        falseButton.setOnClickListener {
-            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP or Gravity.CENTER, 0, 0)
-            toast.show()
-        }
 
         updateQuestion()
 
+        questionText.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+
+        trueButton.setOnClickListener {
+            checkAnswer(true)
+        }
+
+        falseButton.setOnClickListener {
+
+            checkAnswer(false)
+        }
+
+        prevButton.setOnClickListener {
+            currentIndex = if (currentIndex >0)
+                (currentIndex - 1) % questionBank.size
+            else
+                0
+            updateQuestion()
+        }
+
+
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex+1) % questionBank.size
-           updateQuestion()
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
         }
     }
 
-    private fun updateQuestion(){
+    private fun updateQuestion() {
         questionText.setText(questionBank[currentIndex].textResId)
     }
 
-    fun checkAnswer(userAnswer: Boolean ){
-
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
