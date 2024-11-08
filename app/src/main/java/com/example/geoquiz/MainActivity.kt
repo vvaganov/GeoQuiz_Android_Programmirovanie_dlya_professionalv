b966833e7e5c9bee42e7528cad30ff1d67b912df
Valery <i@vvaganov.ru>
2024-11-08 21:48:33 +0500

    PFP 02-str108

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,20 +2,22 @@ package com.example.geoquiz
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.widget.Button
-import android.widget.ImageButton
+
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+
+private const val TAG = "MainActivity!"
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var questionText: TextView
-    private lateinit var nextButton: ImageButton
-    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: Button
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -26,15 +28,17 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true)
     )
     private var currentIndex = 0
+    private var correctAnswerCount = 0
+    private var incorrectAnswerCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bandle?) called ")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         questionText = findViewById(R.id.question_text_view)
-        prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
 
         updateQuestion()
@@ -47,26 +51,54 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener {
             checkAnswer(true)
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
         }
 
         falseButton.setOnClickListener {
-
             checkAnswer(false)
+            falseButton.isEnabled = false
+            trueButton.isEnabled = false
         }
-
-        prevButton.setOnClickListener {
-            currentIndex = if (currentIndex >0)
-                (currentIndex - 1) % questionBank.size
-            else
-                0
-            updateQuestion()
-        }
-
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            if (correctAnswerCount + incorrectAnswerCount == questionBank.size) {
+                questionText.text =
+                    "Вы ответили на все вопросы. $correctAnswerCount правильных ответа и $incorrectAnswerCount не правильных"
+                incorrectAnswerCount = 0
+                correctAnswerCount = 0
+            } else {
+                currentIndex = (currentIndex + 1) % questionBank.size
+                updateQuestion()
+                falseButton.isEnabled = true
+                trueButton.isEnabled = true
+            }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart(Bandle?) called ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume(Bandle?) called ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause(Bandle?) called ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop(Bandle?) called ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy(Bandle?) called ")
     }
 
     private fun updateQuestion() {
@@ -75,10 +107,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
+
+        var messageResId = 0
+        if (userAnswer == correctAnswer) {
+            correctAnswerCount++
+            messageResId = R.string.correct_toast
         } else {
-            R.string.incorrect_toast
+            incorrectAnswerCount++
+            messageResId = R.string.incorrect_toast
+
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
